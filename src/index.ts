715d0cf159fba8a9488e244b03021998b889e6e4
Simona Kruppova <simonak@kontent.ai>
2025-04-15 12:35:38 +0200

    Add tool to get item from mapi

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { z } from "zod";
 import { createDeliveryClient } from '@kontent-ai/delivery-sdk';
+import { createManagementClient } from '@kontent-ai/management-sdk';
 
 // Create server instance
 const server = new McpServer({
@@ -13,10 +14,37 @@ const server = new McpServer({
   },
 });
 
-// Register weather tools
 server.tool(
-  "get-item",
-  "Get Kontent.ai item by codename",
+  "get-item-mapi",
+  "Get Kontent.ai item by codename from Management API",
+  {
+    codename: z.string().describe("Codename of the item to get")
+  },
+  async ({ codename }) => {
+    const client = createManagementClient({
+      apiKey: process.env.KONTENT_API_KEY ?? "",
+      environmentId: process.env.KONTENT_ENVIRONMENT_ID ?? "",
+    });
+
+    const response = await client
+      .viewContentItem()
+      .byItemCodename(codename)
+      .toPromise();
+
+    return {
+      content: [
+        {
+          type: "text",
+          text: JSON.stringify(response.data),
+        },
+      ],
+    };
+  }
+);
+
+server.tool(
+  "get-item-dapi",
+  "Get Kontent.ai item by codename from Delivery API",
   {
     codename: z.string().describe("Codename of the item to get"),
     environmentId: z.string().describe("Environment ID of the item's environment"),
